3cc7e466f04ee1edd3b6ad9fce0b7bff617712ce
MulverineX <mulverine@live.com>
2017-07-11 21:31:41 -0600

    Update itransfer.bat

--- Files/itransfer.bat
+++ Files/itransfer.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist "%appdata%\Mulvpack\" mkdir %appdata%\Mulvpack
-if not exist "%appdata%\Mulvpack\" xcopy /s "%cd%\Mulvpack" "%appdata%\Mulvpack\"
+xcopy /s "%cd%\Mulvpack" "%appdata%\Mulvpack\"
 cd ..
 del Install.lnk
 copy %cd%\Files\Done.txt %cd%
